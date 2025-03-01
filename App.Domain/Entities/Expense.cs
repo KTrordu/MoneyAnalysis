91bfcaed469a4dd6385d21eaf7f71851ce9cf921
KTrordu <alptug.yilmaz@outlook.com>
2025-03-01 21:20:51 +0300

    added the Entities

---
+++ App.Domain/Entities/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Entities
+{
+    public class Expense : BaseEntity
+    {
+        public string ExpenseName { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ExpenseDate { get; set; }
+        public int? ExpenseCategoryId { get; set; }
+        public ExpenseCategory ExpenseCategory { get; set; }
+    }
+}
