3a78127d5742febba696fdb46ac03647ab5d92fa
KTrordu <alptug.yilmaz@outlook.com>
2025-03-03 22:45:58 +0300

    added the IExpenseRepository and ExpenseRepository

---
+++ App.DAL/IRepositories/IExpenseRepository.cs
@@ -0,0 +1,19 @@
+﻿using App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAL.IRepositories
+{
+    public interface IExpenseRepository
+    {
+        Task AddAsync(Expense expense);
+        Task<IEnumerable<Expense>> GetAllAsync();
+        Task<Expense> GetByIdAsync(int id);
+        Task UpdateAsync(Expense expense, int id);
+        Task Delete(Expense expense);
+        Task SaveChangesAsync();
+    }
+}
---
+++ App.DAL/Repositories/ExpenseRepository.cs
@@ -0,0 +1,64 @@
+﻿using App.DAL.IRepositories;
+using App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAL.Repositories
+{
+    public class ExpenseRepository : IExpenseRepository
+    {
+        private readonly AppDbContext _db;
+
+        public ExpenseRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(Expense expense)
+        {
+            await _db.Expenses.AddAsync(expense);
+        }
+
+        public async Task<Expense> GetByIdAsync(int id)
+        {
+            return await _db.Expenses.FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<IEnumerable<Expense>> GetAllAsync()
+        {
+            return await _db.Expenses.ToListAsync();
+        }
+
+        public async Task UpdateAsync(Expense expense, int id)
+        {
+            Expense expenseToUpdate = await GetByIdAsync(id);
+
+            expenseToUpdate.ExpenseName = expense.ExpenseName;
+            expenseToUpdate.Amount = expense.Amount;
+            expenseToUpdate.ExpenseDate = expense.ExpenseDate;
+            expenseToUpdate.ExpenseCategoryId = expense.ExpenseCategoryId;
+            expenseToUpdate.UserId = expense.UserId;
+            expenseToUpdate.ModifiedDate = DateTime.Now;
+
+            _db.Expenses.Update(expenseToUpdate);
+        }
+
+        public Task Delete(Expense expense)
+        {
+            expense.IsDeleted = true;
+            expense.DeletedDate = DateTime.Now;
+            expense.ModifiedDate = DateTime.Now;
+
+            return Task.FromResult(_db.Expenses.Update(expense));
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
