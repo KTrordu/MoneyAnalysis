d0be2e60427dd97e6b8fad488f72f8ff295c0820
KTrordu <alptug.yilmaz@outlook.com>
2025-03-03 23:23:30 +0300

    Added ExpenseDTO

---
+++ App.BLL/DTOs/ExpenseDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.BLL.DTOs
+{
+    public class ExpenseDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string ExpenseName { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public DateTime ExpenseDate { get; set; }
+        [Required]
+        public int ExpenseCategoryId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+    }
+}
