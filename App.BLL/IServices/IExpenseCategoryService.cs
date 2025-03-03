3179c0b85eef36d8f95a0d6e6d6bde2bec8df50c
KTrordu <alptug.yilmaz@outlook.com>
2025-03-04 01:39:20 +0300

    added ExpenseCategoryService and IExpenseCategoryService

---
+++ App.BLL/IServices/IExpenseCategoryService.cs
@@ -0,0 +1,18 @@
+﻿using App.BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.BLL.IServices
+{
+    public interface IExpenseCategoryService
+    {
+        Task AddExpenseCategoryAsync(ExpenseCategoryDTO expenseCategoryDTO);
+        Task<IEnumerable<ExpenseCategoryDTO>> GetAllExpenseCategoriesAsync();
+        Task<ExpenseCategoryDTO> GetExpenseCategoryByIdAsync(int id);
+        Task UpdateExpenseCategoryAsync(ExpenseCategoryDTO expenseCategoryDTO);
+        Task DeleteExpenseCategoryAsync(int id);
+    }
+}
---
+++ App.BLL/Services/ExpenseCategoryService.cs
@@ -0,0 +1,79 @@
+﻿using App.BLL.DTOs;
+using App.BLL.IContexts;
+using App.BLL.IServices;
+using App.DAL.IRepositories;
+using App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.BLL.Services
+{
+    public class ExpenseCategoryService : IExpenseCategoryService
+    {
+        private readonly IExpenseCategoryRepository _expenseCategoryRepository;
+        private readonly IUserContext _userContext;
+
+        public ExpenseCategoryService(IExpenseCategoryRepository expenseCategoryRepository, IUserContext userContext)
+        {
+            _expenseCategoryRepository = expenseCategoryRepository;
+            _userContext = userContext;
+        }
+
+        public async Task AddExpenseCategoryAsync(ExpenseCategoryDTO expenseCategoryDTO)
+        {
+            var expenseCategory = new ExpenseCategory
+            {
+                ExpenseCategoryName = expenseCategoryDTO.ExpenseCategoryName,
+                Id = expenseCategoryDTO.Id
+            };
+
+            await _expenseCategoryRepository.AddAsync(expenseCategory);
+            await _expenseCategoryRepository.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<ExpenseCategoryDTO>> GetAllExpenseCategoriesAsync()
+        {
+            var expenseCategories = await _expenseCategoryRepository.GetAllAsync();
+
+            return expenseCategories.Select(e => new ExpenseCategoryDTO
+            {
+                ExpenseCategoryName = e.ExpenseCategoryName,
+                Id = e.Id
+            });
+        }
+
+        public async Task<ExpenseCategoryDTO> GetExpenseCategoryByIdAsync(int id)
+        {
+            var expenseCategory = await _expenseCategoryRepository.GetByIdAsync(id);
+
+            return new ExpenseCategoryDTO
+            {
+                ExpenseCategoryName = expenseCategory.ExpenseCategoryName,
+                Id = expenseCategory.Id
+            };
+        }
+
+        public async Task UpdateExpenseCategoryAsync(ExpenseCategoryDTO expenseCategoryDTO)
+        {
+            var expenseCategory = new ExpenseCategory
+            {
+                ExpenseCategoryName = expenseCategoryDTO.ExpenseCategoryName,
+                Id = expenseCategoryDTO.Id
+            };
+
+            await _expenseCategoryRepository.UpdateAsync(expenseCategory, expenseCategory.Id);
+            await _expenseCategoryRepository.SaveChangesAsync();
+        }
+
+        public async Task DeleteExpenseCategoryAsync(int id)
+        {
+            var expenseCategory = await _expenseCategoryRepository.GetByIdAsync(id);
+
+            await _expenseCategoryRepository.Delete(expenseCategory);
+            await _expenseCategoryRepository.SaveChangesAsync();
+        }
+    }
+}
