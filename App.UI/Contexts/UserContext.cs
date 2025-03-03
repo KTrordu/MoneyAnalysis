f0f17c80be9fdf05f74685cae3510283457e61d4
KTrordu <alptug.yilmaz@outlook.com>
2025-03-03 23:40:05 +0300

    added UserContext

---
+++ App.UI/Contexts/UserContext.cs
@@ -0,0 +1,23 @@
+﻿using App.BLL.IContexts;
+using System.Security.Claims;
+
+namespace App.UI.Contexts
+{
+    public class UserContext : IUserContext
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserContext(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string UserId =>
+            _httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        public IEnumerable<string> Roles =>
+            _httpContextAccessor.HttpContext?.User?.Claims
+                .Where(c => c.Type == ClaimTypes.Role)
+                .Select(c => c.Value) ?? Enumerable.Empty<string>();
+    }
+}
