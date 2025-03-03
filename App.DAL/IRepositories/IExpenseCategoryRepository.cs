5bc34ce4c366125595f6734e02cdf727b4e4de4c
KTrordu <alptug.yilmaz@outlook.com>
2025-03-03 22:54:05 +0300

    added ExpenseCategoryRepository and IExpenseCategoryRepository

---
+++ App.DAL/IRepositories/IExpenseCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAL.IRepositories
+{
+    public interface IExpenseCategoryRepository
+    {
+        Task AddAsync(ExpenseCategory expenseCategory);
+        Task<IEnumerable<ExpenseCategory>> GetAllAsync();
+        Task<ExpenseCategory> GetByIdAsync(int id);
+        Task UpdateAsync(ExpenseCategory expenseCategory, int id);
+        Task Delete(ExpenseCategory expenseCategory);
+        Task SaveChangesAsync();
+    }
+}
---
+++ App.DAL/Repositories/ExpenseCategoryRepository.cs
@@ -0,0 +1,60 @@
+﻿using App.DAL.IRepositories;
+using App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAL.Repositories
+{
+    public class ExpenseCategoryRepository : IExpenseCategoryRepository
+    {
+        private readonly AppDbContext _db;
+
+        public ExpenseCategoryRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(ExpenseCategory expenseCategory)
+        {
+            await _db.ExpenseCategories.AddAsync(expenseCategory);
+        }
+
+        public async Task<IEnumerable<ExpenseCategory>> GetAllAsync()
+        {
+            return await _db.ExpenseCategories.ToListAsync();
+        }
+
+        public async Task<ExpenseCategory> GetByIdAsync(int id)
+        {
+            return await _db.ExpenseCategories.FirstOrDefaultAsync(ec => ec.Id == id);
+        }
+
+        public async Task UpdateAsync(ExpenseCategory expenseCategory, int id)
+        {
+            ExpenseCategory expenseCategoryToUpdate = await GetByIdAsync(id);
+
+            expenseCategoryToUpdate.ExpenseCategoryName = expenseCategory.ExpenseCategoryName;
+            expenseCategoryToUpdate.ModifiedDate = DateTime.Now;
+
+            _db.ExpenseCategories.Update(expenseCategoryToUpdate);
+        }
+
+        public Task Delete(ExpenseCategory expenseCategory)
+        {
+            expenseCategory.IsDeleted = true;
+            expenseCategory.DeletedDate = DateTime.Now;
+            expenseCategory.ModifiedDate = DateTime.Now;
+
+            return Task.FromResult(_db.ExpenseCategories.Update(expenseCategory));
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
