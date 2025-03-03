d7462096e446b1406417faaae49d85a291a289f7
KTrordu <alptug.yilmaz@outlook.com>
2025-03-03 12:36:56 +0300

    added RoleViewModels

---
+++ App.UI/ViewModels/Role/RoleEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using App.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace App.UI.ViewModels.Role
+{
+    public class RoleEditViewModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+    }
+}
