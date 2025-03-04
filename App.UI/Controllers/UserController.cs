265cf674f126352673af9ac9ab67ab84e93fd7c2
KTrordu <alptug.yilmaz@outlook.com>
2025-03-04 15:42:53 +0300

    added Update for User Panel

--- App.UI/Controllers/UserController.cs
+++ App.UI/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using App.Domain.Entities;
+﻿using App.BLL.DTOs;
+using App.BLL.IServices;
+using App.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,15 +9,44 @@ namespace App.UI.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserService _userService;
 
-        public UserController(UserManager<ApplicationUser> userManager)
+        public UserController(UserManager<ApplicationUser> userManager, IUserService userService)
         {
             _userManager = userManager;
+            _userService = userService;
         }
 
+        //READ: List all users
         public IActionResult Index()
         {
             return View(_userManager.Users);
+        }
+
+        //UPDATE: GET
+        public async Task<IActionResult> Update(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+
+            return View(user);
+        }
+
+        //UPDATE: POST
+        [HttpPost]
+        public async Task<IActionResult> Update(ApplicationUser applicationUser)
+        {
+            var userToUpdate = await _userManager.FindByIdAsync(applicationUser.Id);
+            if (userToUpdate == null) return NotFound();
+
+            userToUpdate.UserName = applicationUser.UserName;
+            await _userService.UpdateUserAsync(new UserDTO
+            {
+                Id = userToUpdate.Id,
+                UserName = userToUpdate.UserName
+            });
+
+            return RedirectToAction("Index");
         }
     }
 }
