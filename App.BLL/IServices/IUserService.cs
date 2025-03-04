21534d4ea6fe1cc8c414616c6a1dc03e69bd135c
KTrordu <alptug.yilmaz@outlook.com>
2025-03-04 14:34:35 +0300

    added UserService and IUserService

---
+++ App.BLL/IServices/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using App.BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.BLL.IServices
+{
+    public interface IUserService
+    {
+        Task AddUserAsync(UserDTO user);
+        Task<IEnumerable<UserDTO>> GetAllUsersAsync();
+        Task<UserDTO> GetUserByIdAsync(string id);
+        Task UpdateUserAsync(UserDTO user);
+        Task DeleteUserAsync(string id);
+    }
+}
---
+++ App.BLL/Services/UserService.cs
@@ -0,0 +1,73 @@
+﻿using App.BLL.DTOs;
+using App.BLL.IServices;
+using App.DAL.IRepositories;
+using App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task AddUserAsync(UserDTO userDTO)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = userDTO.UserName
+            };
+
+            await _userRepository.AddAsync(user);
+            await _userRepository.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<UserDTO>> GetAllUsersAsync()
+        {
+            var users = await _userRepository.GetAllAsync();
+
+            return users.Select(u => new UserDTO
+            {
+                UserName = u.UserName,
+                Id = u.Id
+            });
+        }
+
+        public async Task<UserDTO> GetUserByIdAsync(string id)
+        {
+            var user = await _userRepository.GetByIdAsync(id);
+
+            return new UserDTO
+            {
+                UserName = user.UserName,
+                Id = user.Id
+            };
+        }
+
+        public async Task UpdateUserAsync(UserDTO user)
+        {
+            var userToUpdate = new ApplicationUser
+            {
+                Id = user.Id,
+                UserName = user.UserName
+            };
+
+            await _userRepository.UpdateAsync(userToUpdate);
+            await _userRepository.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(string id)
+        {
+            await _userRepository.Delete(id);
+            await _userRepository.SaveChangesAsync();
+        } 
+    }
+}
