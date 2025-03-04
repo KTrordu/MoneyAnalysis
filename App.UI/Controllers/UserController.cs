54d91474688d33b27c50bff7ae4c2b7305053146
KTrordu <alptug.yilmaz@outlook.com>
2025-03-04 13:42:47 +0300

    added UserController and User Panel

---
+++ App.UI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using App.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_userManager.Users);
+        }
+    }
+}
