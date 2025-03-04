c67fbfb2bab6d6cb4e97ce75ef16f271db523457
KTrordu <alptug.yilmaz@outlook.com>
2025-03-04 14:22:30 +0300

    added UserRepository and IUserRepository

---
+++ App.DAL/IRepositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAL.IRepositories
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(ApplicationUser user);
+        Task<IEnumerable<ApplicationUser>> GetAllAsync();
+        Task<ApplicationUser> GetByIdAsync(string id);
+        Task UpdateAsync(ApplicationUser user);
+        Task Delete(string id);
+    }
+}
---
+++ App.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using App.DAL.IRepositories;
+using App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _db;
+
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(ApplicationUser user)
+        {
+            await _db.Users.AddAsync(user);
+        }
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllAsync()
+        {
+            return await _db.Users.ToListAsync();
+        }
+
+        public async Task<ApplicationUser> GetByIdAsync(string id)
+        {
+            return await _db.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task UpdateAsync(ApplicationUser user)
+        {
+            var userToUpdate = await _db.Users.FirstOrDefaultAsync(u => u.Id == user.Id);
+
+            userToUpdate.UserName = user.UserName;
+            _db.Users.Update(userToUpdate);
+
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(string id)
+        {
+            var userToDelete = await _db.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+            _db.Users.Remove(userToDelete);
+
+            await _db.SaveChangesAsync();
+        }
+    }
+}
