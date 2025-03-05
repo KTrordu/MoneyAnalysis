ce9841528185d8511e4d695cc4e8cf3f589aed41
KTrordu <alptug.yilmaz@outlook.com>
2025-03-05 23:21:48 +0300

    deleted unused ViewModel

--- App.UI/ViewModels/Expense/ExpenseListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace App.UI.ViewModels.Expense
-{
-    public class ExpenseListViewModel
-    {
-        public int ExpenseCategoryId { get; set; }
-        public string ExpenseCategoryName { get; set; }
-        public List<ExpenseViewModel> Expenses { get; set; }
-    }
-}
